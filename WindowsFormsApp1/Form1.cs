6f28b475022682974f2769f7c4cc7f1f78cf53ef
刘伟强 <a1399677960@foxmail.com>
2019-01-08 21:51:16 +0800

    fixed bug

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,18 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("YYSB");
+            this.BackgroundImage = Image.FromFile(Application.StartupPath+@"\1234.png");
+            this.Width =325;
+            this.Height = 678;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
